7978f86ca14391141febc1428a0c57d0afc58c8e
Leo Romanovsky <leoromanovsky@users.noreply.github.com>
2024-05-30 08:58:14 -0700

    Allow a single process to perform network fetching and updating configuration cache (FF-2238) (#24)
    * Add thread-safety to writing and reading from flagConfigs attribute of ConfigurationStore (FF-2238)
    
    * v1.2.1
    
    * fix indent

--- Sources/eppo/ConfigurationStore.swift
+++ Sources/eppo/ConfigurationStore.swift
@@ -1,25 +1,45 @@
-class ConfigurationStore {
-    private let requester: ConfigurationRequester;
-    private var flagConfigs: RACConfig?
+import Foundation
 
+class ConfigurationStore {
+    private let requester: ConfigurationRequester
+    private var flagConfigs: RACConfig?
+    private let syncQueue = DispatchQueue(
+        label: "com.eppo.configurationStoreQueue", attributes: .concurrent)
+    
     public init(requester: ConfigurationRequester) {
-        self.requester = requester;
+        self.requester = requester
         self.flagConfigs = RACConfig(flags: [:])
     }
 
     public func fetchAndStoreConfigurations() async throws {
-        self.flagConfigs = try await self.requester.fetchConfigurations();
+        let config = try await self.requester.fetchConfigurations()
+        self.setConfigurations(config: config)
     }
 
+    // Get the configuration for a given flag key in a thread-safe manner.
+    //
+    // The use of a syncQueue ensures that this read operation is thread-safe and doesn't cause
+    // race conditions where reads could see a partially updated state.
     public func getConfiguration(flagKey: String) -> FlagConfig? {
-        return flagConfigs?.flags[flagKey];
+        return syncQueue.sync {
+            flagConfigs?.flags[flagKey]
+        }
     }
-
-    public func setConfiguration(flagKey: String, config: FlagConfig) {
-        flagConfigs?.flags[flagKey] = config
+    
+    // Set the configurations in a thread-safe manner.
+    //
+    // The use of a barrier ensures that this write operation completes before any other read or write
+    // operations on the `flagConfigs` can proceed. This guarantees that the configuration state is
+    // consistent and prevents race conditions where reads could see a partially updated state.
+    public func setConfigurations(config: RACConfig) {
+        syncQueue.async(flags: .barrier) {
+            self.flagConfigs = config
+        }
     }
-
+    
     public func isInitialized() -> Bool {
-        return flagConfigs?.flags.isEmpty == false
+        return syncQueue.sync {
+            flagConfigs?.flags.isEmpty == false
+        }
     }
 }
--- Tests/eppo/ConfigurationStoreTests.swift
+++ Tests/eppo/ConfigurationStoreTests.swift
@@ -1,29 +1,43 @@
 import XCTest
+
 @testable import eppo_flagging
 
 final class ConfigurationStoreTests: XCTestCase {
     var configurationStore: ConfigurationStore!
     var mockRequester: ConfigurationRequester!
+    var configs: RACConfig!
     
-    let emptyFlagConfig = FlagConfig(subjectShards: 0, enabled: false, typedOverrides: [:], rules: [], allocations: [:])
+    let emptyFlagConfig = FlagConfig(
+        subjectShards: 0, enabled: false, typedOverrides: [:], rules: [], allocations: [:])
     
     override func setUpWithError() throws {
         try super.setUpWithError()
         mockRequester = ConfigurationRequester(httpClient: EppoHttpClientMock())
         configurationStore = ConfigurationStore(requester: mockRequester)
+        
+        configs = RACConfig(flags: [
+            "testFlag": emptyFlagConfig
+        ])
     }
     
     func testSetAndGetConfiguration() throws {
-        configurationStore.setConfiguration(flagKey: "testFlag", config: emptyFlagConfig)
+        // Pass the RACConfig object to setConfigurations
+        configurationStore.setConfigurations(config: configs)
         
-        XCTAssertEqual(configurationStore.getConfiguration(flagKey: "testFlag")?.enabled, emptyFlagConfig.enabled)
+        XCTAssertEqual(
+            configurationStore.getConfiguration(flagKey: "testFlag")?.enabled, emptyFlagConfig.enabled)
     }
     
     func testIsInitialized() async throws {
-        XCTAssertFalse(configurationStore.isInitialized(), "Store should not be initialized before fetching configurations")
+        XCTAssertFalse(
+            configurationStore.isInitialized(),
+            "Store should not be initialized before fetching configurations")
         
-        configurationStore.setConfiguration(flagKey: "testFlag", config: emptyFlagConfig)
+        configurationStore.setConfigurations(config: configs)
         
-        XCTAssertTrue(configurationStore.isInitialized(), "Store should be initialized after fetching configurations")
+        XCTAssertTrue(
+            configurationStore.isInitialized(),
+            "Store should be initialized after fetching configurations")
     }
+    
 }
