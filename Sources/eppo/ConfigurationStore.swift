c881c579effce33f9375947126613c572d2b3bd5
Leo Romanovsky <leo@geteppo.com>
2024-05-29 17:34:25 -0700

    fix indent

--- Sources/eppo/ConfigurationStore.swift
+++ Sources/eppo/ConfigurationStore.swift
@@ -10,12 +10,12 @@ class ConfigurationStore {
         self.requester = requester
         self.flagConfigs = RACConfig(flags: [:])
     }
-    
+
     public func fetchAndStoreConfigurations() async throws {
         let config = try await self.requester.fetchConfigurations()
         self.setConfigurations(config: config)
     }
-    
+
     // Get the configuration for a given flag key in a thread-safe manner.
     //
     // The use of a syncQueue ensures that this read operation is thread-safe and doesn't cause
@@ -26,11 +26,11 @@ class ConfigurationStore {
         }
     }
     
-    //   Set the configurations in a thread-safe manner.
+    // Set the configurations in a thread-safe manner.
     //
-    //   The use of a barrier ensures that this write operation completes before any other read or write
-    //   operations on the `flagConfigs` can proceed. This guarantees that the configuration state is
-    //   consistent and prevents race conditions where reads could see a partially updated state.
+    // The use of a barrier ensures that this write operation completes before any other read or write
+    // operations on the `flagConfigs` can proceed. This guarantees that the configuration state is
+    // consistent and prevents race conditions where reads could see a partially updated state.
     public func setConfigurations(config: RACConfig) {
         syncQueue.async(flags: .barrier) {
             self.flagConfigs = config
