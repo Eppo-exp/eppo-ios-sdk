05fef02775f26ae96c297301e8d784c9a9964897
Oleksii Shmalko <oleksii@fluxon.com>
2024-09-17 10:37:32 +0300

    chore: bump minor version

--- Sources/eppo/EppoClient.swift
+++ Sources/eppo/EppoClient.swift
@@ -2,7 +2,7 @@ import Foundation;
 
 // todo: make this a build argument (FF-1944)
 public let sdkName = "ios"
-public let sdkVersion = "3.1.0"
+public let sdkVersion = "3.2.0"
 
 public let defaultHost = "https://fscdn.eppo.cloud"
 
