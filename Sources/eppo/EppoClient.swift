587f3ba89138b3b2a27dadd1886f98b72ba2b316
Leo Romanovsky <leo@geteppo.com>
2024-10-15 15:15:20 -0700

    bump version to 3.3.1

--- Sources/eppo/EppoClient.swift
+++ Sources/eppo/EppoClient.swift
@@ -2,7 +2,7 @@ import Foundation;
 
 // todo: make this a build argument (FF-1944)
 public let sdkName = "ios"
-public let sdkVersion = "3.3.0"
+public let sdkVersion = "3.3.1"
 
 public let defaultHost = "https://fscdn.eppo.cloud"
 
