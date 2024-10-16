b83a98d0f8dc5de76ee59756e73821fb1321a53e
Leo Romanovsky <leoromanovsky@users.noreply.github.com>
2024-10-15 20:06:55 -0700

    bug: replace usages of DispatchQueue.asyncAndWait with DispatchQueue async. (FF-3331) (#47)

--- Sources/eppo/ConfigurationStore.swift
+++ Sources/eppo/ConfigurationStore.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 class ConfigurationStore {
+    // CAUTION! 
+    // 
+    // Take care before changing the DispatchQueue methods to `asyncOrWait`
+    // as we previously had a bug related to support across older iOS versions.
+    // https://github.com/Eppo-exp/eppo-ios-sdk/issues/46
+
     private var configuration: Configuration?
     private let syncQueue = DispatchQueue(
         label: "com.eppo.configurationStoreQueue", attributes: .concurrent)
@@ -65,7 +71,7 @@ class ConfigurationStore {
     // operations on the `flagConfigs` can proceed. This guarantees that the configuration state is
     // consistent and prevents race conditions where reads could see a partially updated state.
     public func setConfiguration(configuration: Configuration) {
-        syncQueue.asyncAndWait(flags: .barrier) {
+        syncQueue.sync(flags: .barrier) {
             self.configuration = configuration
             self.saveToDisk(configuration: configuration)
         }
@@ -76,7 +82,7 @@ class ConfigurationStore {
             return
         }
 
-        Self.persistenceQueue.asyncAndWait {
+        Self.persistenceQueue.sync {
             do {
                 try FileManager.default.removeItem(at: cacheFileURL)
             } catch {
