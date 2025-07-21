138a1212b5e5644666a21efd01fad22a7ebca915
Sameeran.Kunche <sameeran.kunche@datadoghq.com>
2025-07-21 09:26:47 -0700

    chore: bump SDK version to 4.4.0
    Update SDK version from 4.3.0 to 4.4.0 in Constants.swift

--- Sources/eppo/Constants.swift
+++ Sources/eppo/Constants.swift
@@ -1,5 +1,5 @@
 // todo: make this a build argument (FF-1944)
 public let sdkName = "ios"
-public let sdkVersion = "4.3.0"
+public let sdkVersion = "4.4.0"
 
 public let defaultHost = "https://fscdn.eppo.cloud/api"
