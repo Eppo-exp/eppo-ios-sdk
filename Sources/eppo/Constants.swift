b76ef149e5bba6285e65f6fac5e358648691dc08
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2026-02-09 13:11:14 -0500

    chore: bump version to 5.3.3

--- Sources/eppo/Constants.swift
+++ Sources/eppo/Constants.swift
@@ -1,6 +1,6 @@
 // todo: make this a build argument (FF-1944)
 public let sdkName = "ios"
-public let sdkVersion = "5.3.2"
+public let sdkVersion = "5.3.3"
 
 public let defaultHost = "https://fscdn.eppo.cloud/api"
 public let precomputedBaseUrl = "https://fs-edge-assignment.eppo.cloud"
