95d6d6f92508fc8127533695a8adc50835f00188
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2026-03-11 21:03:29 -0700

    fix: handle missing extraLogging field in precomputed flag JSON
    Add custom decoder to default extraLogging to empty dictionary when
    the field is absent in the API response.

--- Sources/eppo/dto/PrecomputedFlag.swift
+++ Sources/eppo/dto/PrecomputedFlag.swift
@@ -7,7 +7,7 @@ struct PrecomputedFlag: Codable, Equatable {
     let variationValue: EppoValue
     let extraLogging: [String: String]
     let doLog: Bool
-    
+
     init(
         allocationKey: String?,
         variationKey: String?,
@@ -22,6 +22,25 @@ struct PrecomputedFlag: Codable, Equatable {
         self.variationValue = variationValue
         self.extraLogging = extraLogging
         self.doLog = doLog
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        allocationKey = try container.decodeIfPresent(String.self, forKey: .allocationKey)
+        variationKey = try container.decodeIfPresent(String.self, forKey: .variationKey)
+        variationType = try container.decode(VariationType.self, forKey: .variationType)
+        variationValue = try container.decode(EppoValue.self, forKey: .variationValue)
+        extraLogging = try container.decodeIfPresent([String: String].self, forKey: .extraLogging) ?? [:]
+        doLog = try container.decode(Bool.self, forKey: .doLog)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case allocationKey
+        case variationKey
+        case variationType
+        case variationValue
+        case extraLogging
+        case doLog
     }
 }
 
